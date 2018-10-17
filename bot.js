a7a1282bfc850e7d591e7f285e973a384e4f3510
jannikg <jannikg@student.ethz.ch>
2018-10-17 23:59:30 +0200

    done

--- bot.js
+++ bot.js
@@ -1,6 +1,17 @@
+/*
+  To get everything running follow these 2 simple Steps
+*/
+
+//import node-telegram-bot-api
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the values below
+/*
+  *TODO Step1 : Change these values
+
+  *Token is the token you get from botfather
+  *URL is the Url of the rss-feed you want to watch
+  *Intervall is the amount of seconds the watcher sleeps
+*/
 const token = "";   //bot token
 const URL = "";     //url to rss feed
 const INTERVALL = ; //update intervall in s
@@ -8,29 +19,41 @@ const INTERVALL = ; //update intervall in s
 // Create a bot
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
+//simple ping command that reurns pong
 bot.onText(/\/ping/, (msg, match) => {
   const chatId = msg.chat.id;
-  const resp = "Hello";
+  const resp = "pong";
 
   bot.sendMessage(chatId, resp);
 });
 
+//this command is used to subscribe to the feed
+//only subscribed users will get notifications on new entries
 bot.onText(/\/sub/, (msg) => {
   const user = msg.chat.id;
   addUser(String(user));
 });
 
+//this command is used to unsubscribe to the feed
 bot.onText(/\/unsub/, (msg) => {
   const user = msg.chat.id;
   removeUser(String(user));
 });
 
+//this command returns the last 5 entries from the feed
 bot.onText(/\/top/, (msg) => {
   const user = msg.chat.id;
   getTop5Entries(String(user));
 });
 
+//create users.txt if it doesnt exist yet
+var fs = require('fs');
+if (!fs.existsSync('users.txt')) {
+    fs.appendFile('users.txt', '');
+    console.log('created users.txt');
+}
+
+//init new rss watcher
 var Watcher    = require('feed-watcher'),
 feed     = URL,
 interval = INTERVALL // seconds
@@ -55,6 +78,7 @@ interval = INTERVALL // seconds
       //console.error(error)
     })
 
+//adds userId to users.txt
 function addUser (user) {
   var fs = require('fs');
   var users = fs.readFileSync('users.txt', {encoding: 'utf8', flag: 'r'}).toString().split("\n");
@@ -69,6 +93,7 @@ function addUser (user) {
   }
 }
 
+//removes userId from users.txt
 function removeUser (user) {
   var fs = require('fs');
   var users = fs.readFileSync('users.txt', {encoding: 'utf8', flag: 'r'}).toString().split("\n");
@@ -85,6 +110,7 @@ function removeUser (user) {
 }
 
 
+//gets all users from users.txt and sends a message to All
 function sendToAll (message) {
   var fs = require('fs');
   var users = fs.readFileSync('users.txt', {encoding: 'utf8', flag: 'r'}).toString().split("\n");
@@ -96,6 +122,7 @@ function sendToAll (message) {
   }
 }
 
+//get last 5 item of the feed
 function getTop5Entries (user) {
   let Parser = require('rss-parser');
   let parser = new Parser();
@@ -113,7 +140,12 @@ function getTop5Entries (user) {
 })();
 }
 
-//TODO add whatever you want
+/*
+
+  *TODO STEP 2 : customize response message
+  *This is what the bot is going to say whenever a new entry in the feed is found
+  with entry.title for example you can access the title field of a item in the feed
+*/
 function parseEntry(entry) {
   response = "<b>New Chapter: " + entry.title + "\n </b>";
   response += entry.link;
